c7afeb50bae0cb5c8ac557c6cc911a59018037ad
MillenaQueiroz <millena.queiroz08@gmail.com>
2023-06-17 18:31:18 -0300

    add dicionario de propriedades Co-authored-by: Mayara-tech <amayara939@gmail.com>

---
+++ dicionario_propriedades.py
@@ -0,0 +1,30 @@
+caracteristicas = {
+    'mexicano': {
+        'ingredientes': ['pimenta', 'abacate', 'feijão', 'milho', 'coentro'],
+        'caracteristicas': ['picante', 'saboroso', 'colorido']
+    },
+    'japonês': {
+        'ingredientes': ['peixe cru', 'arroz', 'alga nori', 'molho de soja', 'wasabi'],
+        'caracteristicas': ['fresco', 'saudável', 'delicado']
+    },
+    'hipster': {
+        'ingredientes': ['quinoa', 'abacate', 'tempeh', 'cenoura roxa', 'couve-de-bruxelas'],
+        'caracteristicas': ['orgânico', 'artesanal', 'criativo']
+    },
+    'fast food': {
+        'ingredientes': ['hambúrguer', 'batata frita', 'refrigerante', 'queijo', 'molho especial'],
+        'caracteristicas': ['rápido', 'conveniente', 'saboroso']
+    },
+    'brasileira': {
+        'ingredientes': ['feijão', 'arroz', 'carne de porco', 'mandioca', 'farofa'],
+        'caracteristicas': ['saboroso', 'variado', 'rica em sabores']
+    },
+    'italiana': {
+        'ingredientes': ['massa', 'queijo parmesão', 'tomate', 'azeite de oliva', 'manjericão'],
+        'caracteristicas': ['autêntico', 'sabores intensos', 'tradição']
+    },
+    'indiana': {
+        'ingredientes': ['curry', 'gengibre', 'iogurte', 'masala', 'pimenta'],
+        'caracteristicas': ['aromático', 'picante', 'exótico']
+    }
+}
