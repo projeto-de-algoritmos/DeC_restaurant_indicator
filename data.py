12f91c17aeca403fa4dd5ce95024a82809832589
MillenaQueiroz <millena.queiroz08@gmail.com>
2023-06-17 18:25:20 -0300

    Add restaurant data

---
+++ data.py
@@ -0,0 +1,100 @@
+restaurantes = {
+    'mexicano': [
+        {
+            'nome': 'Si Senor',
+            'link': 'https://www.sisenor.com.br/'
+        },
+        {
+            'nome': 'El Paso Texas',
+            'link': 'https://www.elpaso.com.br/'
+        },
+        {
+            'nome': 'Hermanito',
+            'link': 'https://www.instagram.com/hermanito_asanorte/'
+        }
+    ],
+    'japones': [
+        {
+            'nome': 'Nippon',
+            'link': 'https://nipponbrasilia.com.br/'
+        },
+        {
+            'nome': 'Restaurante Kojima',
+            'link': 'https://www.restaurantekojima.com.br/'
+        },
+        {
+            'nome': 'Shoio Sushi Lounge',
+            'link': 'https://www.shoiosushi.com/'
+        }
+    ],
+    'hipster': [
+        {
+            'nome': 'Resenha Bar e Restaurante',
+            'link': 'https://www.facebook.com/resenhabarerestaurante'
+        },
+        {
+            'nome': 'Green House',
+            'link': 'https://www.greenhouselago.com/'
+        },
+        {
+            'nome': 'Le Birosque',
+            'link': 'https://www.facebook.com/lebirosque'
+        }
+    ],
+    'fastfood': [
+        {
+            'nome': 'McDonald\'s',
+            'link': 'https://www.mcdonalds.com.br/'
+        },
+        {
+            'nome': 'Burger King',
+            'link': 'https://www.burgerking.com.br/'
+        },
+        {
+            'nome': 'Subway',
+            'link': 'https://www.subway.com.br/'
+        }
+    ],
+    'brasileira': [
+        {
+            'nome': 'Mangai',
+            'link': 'https://mangai.com.br/'
+        },
+        {
+            'nome': 'Tapera Brasileira Restaurante',
+            'link': 'https://restaurantguru.com.br/Tapera-Brasileira-Brasilia'
+        },
+        {
+            'nome': 'Fogão Mineiro',
+            'link': 'https://www.fogaomineiro.com.br/'
+        }
+    ],
+    'italiana': [
+        {
+            'nome': 'Papa Cucina',
+            'link': 'https://www.instagram.com/papacucina_/'
+        },
+        {
+            'nome': 'Villa Tevere',
+            'link': 'hhttps://www.villatevere.com.br/'
+        },
+        {
+            'nome': 'A Mano',
+            'link': 'https://www.instagram.com/amanorestaurante/'
+        }
+    ],
+    'indiana': [
+        {
+            'nome': 'Indian House',
+            'link': 'https://www.hubt.com.br/indian-house/'
+        },
+        {
+            'nome': 'Namaste Restaurante',
+            'link': 'https://www.ifood.com.br/delivery/brasilia-df/namaste-indian-restaurante-asa-norte/c72a513e-18f0-41cb-8af6-1e9f21ef5500'
+        },
+        {
+            'nome': 'Indian Lounge',
+            'link': 'https://www.facebook.com/indianloungebsb/'
+        }
+    ]
+}
