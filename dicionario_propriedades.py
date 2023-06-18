08244c298e2885a588e14c95f90508711c9d5552
MillenaQueiroz <millena.queiroz08@gmail.com>
2023-06-18 12:22:54 -0300

    Add array de genero
    Co-authored-by: Mayara-tech <amayara939@gmail.com>

--- dicionario_propriedades.py
+++ dicionario_propriedades.py
@@ -1,8 +1,32 @@
+ingredientes = {
+    1: 'pimenta',
+    2: 'legumes',
+    3: 'carne',
+    4: 'massas',
+    5: 'arroz'
+}
+
 caracteristicas = {
-    'mexicano': ['pimenta', 'legumes', 'carne', 'massas', 'arroz'],
-    'japonês': ['arroz', 'legumes', 'carne', 'massas', 'pimenta'],
-    'italiano': ['massas', 'legumes', 'carne', 'pimenta', 'arroz'],
+    'mexicana': ['pimenta', 'legumes', 'carne', 'massas', 'arroz'],
+    'japonesa': ['arroz', 'legumes', 'carne', 'massas', 'pimenta'],
+    'italiana': ['massas', 'legumes', 'carne', 'pimenta', 'arroz'],
     'indiana': ['massas', 'arroz', 'legumes', 'carne', 'pimenta'],
     'brasileira': ['legumes', 'carne', 'massas', 'pimenta', 'arroz'],
     'churrascaria': ['carne', 'pimenta', 'legumes', 'massas', 'arroz']
 }
+
+
+##Transformando o dicionario em array de id
+mexicana = [id for id in ingredientes if ingredientes[id] in caracteristicas['mexicana']]
+brasileira = [id for id in ingredientes if ingredientes[id] in caracteristicas['brasileira']]
+churrascaria = [id for id in ingredientes if ingredientes[id] in caracteristicas['churrascaria']]
+italiana = [id for id in ingredientes if ingredientes[id] in caracteristicas['italiana']]
+indiana = [id for id in ingredientes if ingredientes[id] in caracteristicas['indiana']]
+japonesa = [id for id in ingredientes if ingredientes[id] in caracteristicas['japonesa']]
+
+# print('Array da culinária mexicana:', mexicana)
+# print('Array da culinária brasileira:', brasileira)
+# print('Array da culinária churrascaria:', churrascaria)
+# print('Array da culinária italiana:', italiana)
+# print('Array da culinária indiana:', indiana)
+# print('Array da culinária japonesa:', japonesa)
